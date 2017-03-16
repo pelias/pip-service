8e4d4abaa2adb062bf43231a9b712e0c10d53768
Stephen Hess <stephen.k.hess@gmail.com>
2017-03-16 16:21:18 -0400

    added validation of WOF data directory + tests

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
-FROM ubuntu:14.04
+FROM ubuntu:16.04
 
 # dependencies (for node)
-RUN apt-get update && apt-get install -y curl
+RUN apt-get update && apt-get install -y git-core curl bzip2
 RUN curl -sL https://deb.nodesource.com/setup_4.x | bash -
 
 # rest of dependencies
 RUN apt-get update && apt-get install -y nodejs
+RUN npm install -g npm
 
 # copy in the pip-service code
 WORKDIR /pip
@@ -15,9 +16,5 @@ RUN npm install
 # creates a directory called data
 VOLUME /whosonfirst
 
-RUN echo "starting pip service"
-
-# start the pip service
+# start the pip service using the directory the data was downloaded to
 CMD ["npm", "start", "--", "/whosonfirst"]
-
-RUN echo "started pip service"
--- test/app.js
+++ test/app.js
@@ -1,69 +1,61 @@
 const tape = require('tape');
 const request = require('request');
 const proxyquire = require('proxyquire').noCallThru();
+const fs = require('fs');
+const path = require('path');
+const temp = require('temp').track();
 
 tape('entry point tests', (test) => {
   test.test('valid lat/lon should call lookup and return result', (t) => {
-    const app = proxyquire('../app', {
-      'pelias-wof-admin-lookup': {
-        resolver: (datapath) => {
-          t.equals(datapath, 'this is the datapath');
-          return {
-            lookup: (centroid, layers, callback) => {
-              t.deepEquals(centroid, { lat: 12.121212, lon: 21.212121 });
-              t.deepEquals(layers, undefined);
-              callback(undefined, 'this is the result');
-            }
-          };
-        }
-      }
-    })('this is the datapath');
-    const server = app.listen();
-    const port = server.address().port;
 
-    request.get(`http://localhost:${port}/21.212121/12.121212`, (err, response, body) => {
+    temp.mkdir('whosonfirst', (err, temp_dir) => {
       t.notOk(err);
-      t.equals(response.statusCode, 200);
-      t.equals(body, 'this is the result');
-      t.end();
-      server.close();
+
+      fs.mkdirSync(path.join(temp_dir, 'data'));
+      fs.mkdirSync(path.join(temp_dir, 'meta'));
+
+      const app = proxyquire('../app', {
+        'pelias-wof-admin-lookup': {
+          resolver: (datapath) => {
+            t.equals(datapath, temp_dir);
+            return {
+              lookup: (centroid, layers, callback) => {
+                t.deepEquals(centroid, { lat: 12.121212, lon: 21.212121 });
+                t.deepEquals(layers, undefined);
+                callback(undefined, 'this is the result');
+              }
+            };
+          }
+        }
+      })(temp_dir);
+      const server = app.listen();
+      const port = server.address().port;
+
+      request.get(`http://localhost:${port}/21.212121/12.121212`, (err, response, body) => {
+        t.notOk(err);
+        t.equals(response.statusCode, 200);
+        t.equals(body, 'this is the result');
+        t.end();
+        server.close();
+        temp.cleanupSync();
+
+      });
 
     });
 
   });
 
   test.test('request not matching desired path should return 404', (t) => {
-    const app = proxyquire('../app', {
-      'pelias-wof-admin-lookup': {
-        resolver: (datapath) => {
-          t.equals(datapath, 'this is the datapath');
-          return {
-            lookup: () => {
-              throw Error('lookup should not have been called');
-            }
-          };
-        }
-      }
-    })('this is the datapath');
-    const server = app.listen();
-    const port = server.address().port;
-
-    request.get(`http://localhost:${port}/21.212121`, (err, response, body) => {
+    temp.mkdir('whosonfirst', (err, temp_dir) => {
       t.notOk(err);
-      t.equals(response.statusCode, 404);
-      t.end();
-      server.close();
 
-    });
+      fs.mkdirSync(path.join(temp_dir, 'data'));
+      fs.mkdirSync(path.join(temp_dir, 'meta'));
 
-  });
-
-  ['a', NaN, Infinity, {}, false, null, undefined].forEach((bad_lat_value) => {
-    test.test('non-finite lat should return 400', (t) => {
       const app = proxyquire('../app', {
         'pelias-wof-admin-lookup': {
           resolver: (datapath) => {
-            t.equals(datapath, 'this is the datapath');
+            t.equals(datapath, temp_dir);
             return {
               lookup: () => {
                 throw Error('lookup should not have been called');
@@ -71,16 +63,55 @@ tape('entry point tests', (test) => {
             };
           }
         }
-      })('this is the datapath');
+      })(temp_dir);
       const server = app.listen();
       const port = server.address().port;
 
-      request.get(`http://localhost:${port}/21.212121/${bad_lat_value}`, (err, response, body) => {
+      request.get(`http://localhost:${port}/21.212121`, (err, response, body) => {
         t.notOk(err);
-        t.equals(response.statusCode, 400);
-        t.equals(body, 'Cannot parse input');
+        t.equals(response.statusCode, 404);
         t.end();
         server.close();
+        temp.cleanupSync();
+
+      });
+
+    });
+
+  });
+
+  ['a', NaN, Infinity, {}, false, null, undefined].forEach((bad_lat_value) => {
+    test.test('non-finite lat should return 400', (t) => {
+      temp.mkdir('whosonfirst', (err, temp_dir) => {
+        t.notOk(err);
+
+        fs.mkdirSync(path.join(temp_dir, 'data'));
+        fs.mkdirSync(path.join(temp_dir, 'meta'));
+
+        const app = proxyquire('../app', {
+          'pelias-wof-admin-lookup': {
+            resolver: (datapath) => {
+              t.equals(datapath, temp_dir);
+              return {
+                lookup: () => {
+                  throw Error('lookup should not have been called');
+                }
+              };
+            }
+          }
+        })(temp_dir);
+        const server = app.listen();
+        const port = server.address().port;
+
+        request.get(`http://localhost:${port}/21.212121/${bad_lat_value}`, (err, response, body) => {
+          t.notOk(err);
+          t.equals(response.statusCode, 400);
+          t.equals(body, 'Cannot parse input');
+          t.end();
+          server.close();
+          temp.cleanupSync();
+
+        });
 
       });
 
@@ -90,27 +121,36 @@ tape('entry point tests', (test) => {
 
   ['a', NaN, Infinity, {}, false, null, undefined].forEach((bad_lon_value) => {
     test.test('non-finite lon should return 400', (t) => {
-      const app = proxyquire('../app', {
-        'pelias-wof-admin-lookup': {
-          resolver: (datapath) => {
-            t.equals(datapath, 'this is the datapath');
-            return {
-              lookup: () => {
-                throw Error('lookup should not have been called');
-              }
-            };
-          }
-        }
-      })('this is the datapath');
-      const server = app.listen();
-      const port = server.address().port;
-
-      request.get(`http://localhost:${port}/${bad_lon_value}/12.121212`, (err, response, body) => {
+      temp.mkdir('whosonfirst', (err, temp_dir) => {
         t.notOk(err);
-        t.equals(response.statusCode, 400);
-        t.equals(body, 'Cannot parse input');
-        t.end();
-        server.close();
+
+        fs.mkdirSync(path.join(temp_dir, 'data'));
+        fs.mkdirSync(path.join(temp_dir, 'meta'));
+
+        const app = proxyquire('../app', {
+          'pelias-wof-admin-lookup': {
+            resolver: (datapath) => {
+              t.equals(datapath, temp_dir);
+              return {
+                lookup: () => {
+                  throw Error('lookup should not have been called');
+                }
+              };
+            }
+          }
+        })(temp_dir);
+        const server = app.listen();
+        const port = server.address().port;
+
+        request.get(`http://localhost:${port}/${bad_lon_value}/12.121212`, (err, response, body) => {
+          t.notOk(err);
+          t.equals(response.statusCode, 400);
+          t.equals(body, 'Cannot parse input');
+          t.end();
+          server.close();
+          temp.cleanupSync();
+
+        });
 
       });
 
@@ -118,5 +158,64 @@ tape('entry point tests', (test) => {
 
   });
 
+  test.test('non-existent whosonfirst directory should throw error', (t) => {
+    t.throws(() => {
+      proxyquire('../app', {
+        'pelias-wof-admin-lookup': {
+          resolver: (datapath) => {
+            throw Error('resolver() should not have been called');
+          }
+        }
+      })('directory_that_does_not_exist');
+
+    }, /directory_that_does_not_exist does not contain Who's on First data/);
+
+    t.end();
+
+  });
+
+  test.test('non-existent whosonfirst/data directory should throw error', (t) => {
+    temp.mkdir('whosonfirst', (err, temp_dir) => {
+      t.throws(() => {
+        fs.mkdirSync(path.join(temp_dir, 'meta'));
+
+        const app = proxyquire('../app', {
+          'pelias-wof-admin-lookup': {
+            resolver: (datapath) => {
+              throw Error('resolver() should not have been called');
+            }
+          }
+        })(temp_dir);
+
+      }, new RegExp(`${temp_dir} does not contain Who's on First data`));
+
+      t.end();
+      temp.cleanupSync();
+
+    });
+
+  });
+
+  test.test('non-existent whosonfirst/meta directory should throw error', (t) => {
+    temp.mkdir('whosonfirst', (err, temp_dir) => {
+      t.throws(() => {
+        fs.mkdirSync(path.join(temp_dir, 'data'));
+
+        const app = proxyquire('../app', {
+          'pelias-wof-admin-lookup': {
+            resolver: (datapath) => {
+              throw Error('resolver() should not have been called');
+            }
+          }
+        })(temp_dir);
+
+      }, new RegExp(`${temp_dir} does not contain Who's on First data`));
+
+      t.end();
+      temp.cleanupSync();
+
+    });
+
+  });
 
 });
