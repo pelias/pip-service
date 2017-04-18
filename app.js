d259e6356a3d17ced191988cae7a0137d42e1842
Stephen Hess <stephen.k.hess@gmail.com>
2017-04-18 11:08:36 -0400

    added comment

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ function log() {
 }
 
 module.exports = (datapath) => {
+  // verify the WOF data structure first (must contain data and meta directories)
   if (!['meta', 'data'].every((sub) => { return fs.existsSync(path.join(datapath, sub)); })) {
     throw Error(`${datapath} does not contain Who's on First data`);
   }
