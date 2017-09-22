68d3cc9faa97b55fdc5b893b6c7e9a21fbbaa83f
Stephen Hess <stephen.k.hess@gmail.com>
2017-09-21 20:23:44 -0400

    added ocean, marinearea, continent, and empire to downloader

--- download_data.sh
+++ download_data.sh
@@ -3,7 +3,7 @@
 pushd /whosonfirst
 mkdir -p meta
 
-for layer in country disputed dependency macroregion region macrocounty county localadmin locality borough neighbourhood
+for layer in ocean marinearea continent empire country disputed dependency macroregion region macrocounty county localadmin locality borough neighbourhood
 do
   echo Fetching $layer bundle
   curl https://whosonfirst.mapzen.com/bundles/wof-$layer-latest-bundle.tar.bz2 | tar --strip-components=1 --exclude=README.txt -xj
