2e639275419801841294709ff24374fa1e0884f0
Julian Simioni <julian@mapzen.com>
2017-11-20 11:48:34 -0500

    Remove all checking of datapath
    This is all done via pelias-wof-admin-lookup now

--- app.js
+++ app.js
@@ -91,12 +91,7 @@ function log() {
   });
 }
 
-module.exports = (datapath) => {
-  // verify the WOF data structure first (must contain data and meta directories)
-  if (!['meta', 'data'].every((sub) => { return fs.existsSync(path.join(datapath, sub)); })) {
-    throw Error(`${datapath} does not contain Who's on First data`);
-  }
-
+module.exports = () => {
   const pointInPoly = adminLookup.localResolver();
 
   const router = new Router();
